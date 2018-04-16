85b4fa6b2388d9ce01f555699fbe8153e1c6dfe4
gcaraman <gcaraman@mitocgroup.com>
2018-04-16 23:15:35 +0300

    Added frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  input: string;
+  domains: any = [];
+  onUserInputChange = new Subject<string>();
+
+  constructor(private http: HttpClient) {
+    this.onUserInputChange
+      .debounceTime(350)
+      .distinctUntilChanged()
+      .subscribe((value) => {
+        this.getDomains(value);
+      });
+  }
+
+  updateDomainList(input) {
+    this.onUserInputChange.next(input);
+  }
+
+  /**
+   * Makes a GET request for avalaible domains`
+   */
+  private getDomains(input: string) {
+    this.http.get(`http://localhost:3000/${input}`).subscribe((value) => { this.domains = value; });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
